fff16318ac1187443e5e9731c80d50c7718fb9f9
nichols12 <nickpontoc@gmail.com>
2017-06-19 09:20:12 -0300

    fweafaew
    fwaefawef

--- scripts/utf8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cont=1;
-for arq in ls /nfs/project/odysci/customers/precifica/casamentos\ a\ melhorar\ 03.2017/CSVs\ da\ Precifica/05.04.2017/*; do
-  iconv -f iso-8859-1 -t utf-8 -o /home/nicholas/Documents/Precifica/Csvutf8/"$cont".csv "$arq"
-  echo "$arq"
-  cont=$(($cont+1));
-done
